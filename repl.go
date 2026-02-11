3de45b87bae2def12c185eff3fb87acef78708d8
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-02-11 16:18:05 +0000

    String tokenizing.

--- repl.go
+++ repl.go
@@ -1,6 +1,11 @@
 package main
 
+import "strings"
+
 func cleanInput(text string) []string {
 	var s []string
+	text = strings.TrimSpace(text)
+	lower := strings.ToLower(text)
+	s = strings.Fields(lower)
 	return s
 }
